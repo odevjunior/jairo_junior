668f2e014019ae3ad73f521e83119ac5b5457398
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-12-06 11:47:50 -0300

    finishing

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,25 @@ import Contacts from './components/Contacts'
 import FatecBox from './components/FatecBox'
 import Career from './components/Career'
 import Footer from './components/Footer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <WhoIAm/>
-      <Knowledge/>
-      <Career/>
-      <Contacts/>
-      <Projects/>      
-      <FatecBox/>
-      <Footer/>
-    </div>
+    <>
+      <Head>
+          <title>Jairo Junior</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>
+        <Header/>
+        <WhoIAm/>
+        <Knowledge/>
+        <Career/>
+        <Contacts/>
+        <Projects/>      
+        <FatecBox/>
+        <Footer/>
+      </div>
+    </>
   )
 }
